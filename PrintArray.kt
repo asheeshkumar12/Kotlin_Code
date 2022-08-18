5b3cf86759a2f8a0cdb3947be105a156690e7d98
Asheesh Kumar <91361484+asheeshkumar12@users.noreply.github.com>
2022-08-19 01:38:59 +0530

    Update PrintArray.kt

--- PrintArray.kt
+++ PrintArray.kt
@@ -1,6 +1,6 @@
-import java.util.*
+import java.util.*  //import libarary for toString function 
 fun main()
 {
     val arrint=arrayOf<Int>(1,2,3,4);
-    println(Arrays.toString(arrint))
+    println(Arrays.toString(arrint)) // toString function directly print the all element of array.
 }
