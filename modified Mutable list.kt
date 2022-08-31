46eedf94dd84497669478daf38b1cbfd62cae63d
Asheesh Kumar <91361484+asheeshkumar12@users.noreply.github.com>
2022-08-31 20:10:29 +0530

    Create modified Mutable list.kt
    In the mutable version of the list in the previous question add the string: “Harry Kane”. From the modified list remove the string: “Wayne Rooney”. Now in this list add the string: “Neymar Jr.” at index 1. Now print the modified mutable list.

---
+++ modified Mutable list.kt
@@ -0,0 +1,10 @@
+fun main() {
+    
+    // write your code here
+    val mutlist=mutableListOf("Lionel Messi","Wayne Rooney","ka Modric","den Hazard","ergio Aguero")
+    mutlist.add("Harry kane")
+    mutlist.removeAt(1)
+    mutlist.add(1,"Neymar Jr.")
+    println(mutlist)
+
+}
